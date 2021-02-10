315a8b3313505d399d67cea7539384299cbc0068
WestMagnolia <37125778+WestMagnolia@users.noreply.github.com>
2021-02-09 22:51:54 -0500

    from laptop

--- django_test/blog/admin.py
+++ django_test/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+#To show posts on admin page
+admin.site.register(Post)
--- django_test/blog/models.py
+++ django_test/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100) #Maxiumu length of title, makes it a character
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #This is for the access to the database, allows for a one- to many relationship
+                                                            #One author, can have many posts.
+    def __str__(self):
+        return self.title
